55b754b0a06bc59b96d8231c2bff4693fa48e836
A-DetyY <13760254546@163.com>
2018-08-13 20:59:33 +0800

    insert sort

--- code/sort.py
+++ code/sort.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 
 def insert_sort(array):
-    pass
+    length = len(array)
+    for j in range(1, length):
+        key = array[j]
+        i = j - 1
+        while i >= 0 and array[i] > key:
+            array[i+1] = array[i]
+            i -= 1
+        array[i+1] = key
+
+
+if __name__ == '__main__':
+    array = [random.randint(0,100) for i in range(20)]
+    print(array)
+    insert_sort(array)
+    print(array)
