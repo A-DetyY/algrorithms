a80736b1a5e8bc7d4d52fd2e7829500debe77160
A-DetyY <13760254546@163.com>
2018-08-15 00:24:08 +0800

    linked list

--- code/linkedlist.py
+++ code/linkedlist.py
@@ -5,68 +5,78 @@ class Node:
     def __init__(self, value=None):
         self.value = value
         self.next = None
-
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next
-
-    def set_value(self, value):
-        self.value = value
-
-    def set_next(self, node):
-        self.next = node
+        self.prev = None
 
 
+# 双向循环链表
 class LinkedList:
     def __init__(self):
         self.head = Node()
+        self.size = 0
 
     def insert_node(self, value):
         node = Node(value)
-        if self.head.get_next() is None:
-            self.head.set_next(node)
+        if self.head.value is None:
+            self.head = node
+            self.head.next = self.head
+            self.head.prev = self.head
         else:
-            node.set_next(self.head.get_next())
-            self.head.set_next(node)
+            """
+            first = self.head
+            for i in range(self.size-1):
+                first = first.next
+            first.next = node
+            node.prev = first
+            node.next = self.head
+            self.head.prev = node
+            """
+            node.next = self.head.next
+            node.prev = self.head
+            self.head.next.prev = node
+            self.head.next = node
+            self.head = node
+        self.size += 1
 
     def delete_node(self, value):
         head = self.head
-        while head.get_next() is not None and head.get_next().get_value() != value:
-            head = head.get_next()
-        if head.get_next() is None:
+        i = 1
+        while head.value != value and i <= self.size:
+            head = head.next
+            i += 1
+        if i > self.size:
             print("not find the node that you want to delete")
         else:
-            head.set_next(head.get_next().get_next())
+            head.prev.next = head.next
+            head.next.prev = head.prev
+            self.size -= 1
 
     def search_node(self, value):
-        temp = self.head.get_next()
-        pos = 1
-        while temp is not None and temp.get_value() != value:
-            temp = temp.get_next()
-            pos += 1
-        if temp is None:
+        temp = self.head
+        i = 1
+        while i <= self.size and temp.value != value:
+            temp = temp.next
+            i += 1
+        if i > self.size:
             print("not find the node that you want to search")
         else:
-            print("this value exist in Node {}".format(pos))
+            print("this value exist in Node {}".format(i))
 
     def modify_node(self, value, replace):
-        temp = self.head.get_next()
-        while temp is not None and temp.get_value() != value:
-            temp = temp.get_next()
+        temp = self.head.next
+        while temp is not None and temp.value != value:
+            temp = temp.next
         if temp is None:
             print("not find the node that you want to modify")
         else:
-            temp.set_value(replace)
+            temp.value = replace
             print("replace {} with {}".format(value, replace))
 
     def display(self):
-        temp = self.head.get_next()
+        temp = self.head
         s = ''
-        while temp is not None:
-            s += "{}  ".format(temp.get_value())
-            temp = temp.get_next()
+        for i in range(self.size):
+            s += "{}  ".format(temp.value)
+            temp = temp.next
         print(s)
 
 
@@ -79,4 +89,6 @@ if __name__ == '__main__':
     linked_list.modify_node(7, 1)
     linked_list.display()
     linked_list.delete_node(4)
+    linked_list.display()
+    linked_list.delete_node(11)
     linked_list.display()
